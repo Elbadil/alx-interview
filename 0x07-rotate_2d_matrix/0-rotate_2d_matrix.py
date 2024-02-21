c8de94b9a60e387eb547070181344937c1dfd657
Elbadil <elbaliadil@gmail.com>
2024-02-21 16:05:42 +0100

    Pycodestyle

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,18 +7,10 @@ def rotate_2d_matrix(matrix):
     temp = [[]]
     N = len(matrix)  # = 3
 
-    # Transposing the matrix changing the columns with the rows
-    for i in range(N):  # i = 0 # i = 1 # i = 2
-        for j in range(i, N):  # j = 0 | = 1 | = 2 // j = 1 | j = 2 // j = 2
-            temp = matrix[i][j]  # 1 # = 2 # = 3 // = 5 | = 6 // = 9
-    # 1) 1 will be changing position with 1
-    # 2) 2 will be changing position with 4 first element in the second list
-    # 3) 3 will be changing position with 7 first element in the third list
-    # ------------------------- i = 1 ----------------------------------------
-    # 1) 5 will be changing position with 5 second element in the second list
-    # 2) 6 will be changing position with 8 second element in the third list
-    # ------------------------- i = 2 ----------------------------------------
-    # 1) 9 will be changing position with 9
+    # Transposing the matrix by converting the rows into columns
+    for i in range(N):
+        for j in range(i, N):
+            temp = matrix[i][j]
             matrix[i][j] = matrix[j][i]
             matrix[j][i] = temp
 
