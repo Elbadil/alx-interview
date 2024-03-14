0a00a0567f445e547355bdcdf4efadf05a11059c
Elbadil <elbaliadil@gmail.com>
2024-03-14 23:54:43 +0000

    Third Attempt

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,9 +2,7 @@
 """Defining a function isWinner"""
 
 
-def isWinner(x, nums):
-    """Returns the winner of the prime game of numbers"""
-    def is_prime(n):
+def is_prime(n):
         """Returns true if n is a prime number else False"""
         if n < 2:
             return False
@@ -13,28 +11,32 @@ def isWinner(x, nums):
                 return False
         return True
 
+
+def count_primes(n):
+    """Counts the number of prime numbers up to n"""
+    count = 0
+    for i in range(2, n + 1):
+        if is_prime(i):
+            count += 1
+    return count
+
+
+def isWinner(x, nums):
+    """Returns the winner of the prime game of numbers"""
     if x < 1 or not nums:
         return None
 
     maria_ws = 0
     ben_ws = 0
     for n in nums:
-        # If n is even, Ben wins since Maria can't pick a prime
-        if n % 2 == 0:
+        primes_count = count_primes(n)
+        if primes_count % 2 == 0:
             ben_ws += 1
-        # If n is odd and prime, Maria wins since she picks the
-        # only available prime
-        elif is_prime(n):
-            maria_ws += 1
-        # If n is odd but not prime
-        # Ben wins since he removes the prime
-        # factors and leaves Maria with 1
         else:
-            ben_ws += 1
+            maria_ws += 1
 
     if maria_ws > ben_ws:
-        return "Maria"
+        return 'Maria'
     elif ben_ws > maria_ws:
-        return "Ben"
-    else:
-        return None
+        return 'Ben'
+    return None
