b28d6563e3d8f42485a5ce56227b7aa113aef592
Elbadil <elbaliadil@gmail.com>
2024-03-14 23:59:36 +0000

    pycodestyle

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,13 +3,13 @@
 
 
 def is_prime(n):
-        """Returns true if n is a prime number else False"""
-        if n < 2:
+    """Returns true if n is a prime number else False"""
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
             return False
-        for i in range(2, int(n ** 0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
+    return True
 
 
 def count_primes(n):
